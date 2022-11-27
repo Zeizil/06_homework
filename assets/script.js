90aab7485fed418c3c4969c9acd0ac2867e10a84
Zeizil <ekphilbrook@gmail.com>
2022-11-27 13:08:50 -0500

    more javascript, fixed api key and fetch

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,56 @@
+const cityEl = document.getElementById("citysearch");
+const srchBtn = document.getElementById("fetch-button");
 
-
-function getApi() {
-    const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`;
+function getApi(lat, lon) {
+    const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${ApiKey}`;
 
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
+        .then(function (data) {
+             console.log(data);
+             //data[0].list
+        })
 }
+
+function getCurrentWeather(lat, lon) {
+    const requestUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${ApiKey}`
+
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+         console.log(data);
+         //data[0].weather
+         //data[0].list
+         // etc
+    })
+}
+
+function getLatLon(city) {
+    const requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${ApiKey}`;
+    
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+         console.log(data);
+         const lat = data[0].lat;
+         const lon = data[0].lon;
+         //getApi, pass lat and lon
+         getCurrentWeather(lat, lon);
+         getApi(lat, lon);
+    })
+}
+
+function citySearch() {
+    // get the city name from input
+    const city = cityEl.value;
+    getLatLon(city);
+}
+
+// event listener for search button
+srchBtn.addEventListener('click', citySearch);
